5fe35572d7e471d57d8f335508f3f11039ef7e0e
Егор Бардацкиий <yegor_bardatsky@MacBook-Air-Egor.local>
2024-01-20 23:57:00 +0300

    added 2nd way of saving title input

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -8,11 +8,17 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  var _enteredTitle = '';
-  void _saveTitleInput(String inputValue) {
-    _enteredTitle = inputValue;
-  }
+  //var _enteredTitle = '';
+  // void _saveTitleInput(String inputValue) {
+  //   _enteredTitle = inputValue;
+  // }
+  final _titleController = TextEditingController();
 
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -20,7 +26,8 @@ class _NewExpenseState extends State<NewExpense> {
       child: Column(
         children: [
           TextField(
-            onChanged: _saveTitleInput,
+            //onChanged: _saveTitleInput,
+            controller: _titleController,
             maxLength: 50,
             keyboardType: TextInputType.text,
             decoration: const InputDecoration(
@@ -31,7 +38,7 @@ class _NewExpenseState extends State<NewExpense> {
             children: [
               ElevatedButton(
                 onPressed: () {
-                  print(_enteredTitle);
+                  print(_titleController.text);
                 },
                 child: const Text('Save Expense'),
               ),
