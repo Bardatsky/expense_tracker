ea682db7c446cf6f84de5626f835e5d3d7b8d3a0
Егор Бардацкиий <yegor_bardatsky@MacBook-Air-Egor.local>
2024-01-21 21:35:04 +0300

    imlemented function to add expenses to the expenses list

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -5,7 +5,7 @@ import 'package:expense_tracker/models/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
-
+  
   @override
   State<Expenses> createState() => _ExpensesState();
 }
@@ -29,8 +29,14 @@ class _ExpensesState extends State<Expenses> {
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
       context: context,
-      builder: (ctx) => const NewExpense(),
+      builder: (ctx) => NewExpense(onAddExpense: _addExpense),
     );
+  }
+
+  void _addExpense(Expense expense){
+    setState(() {
+      _registredExpenses.add(expense);
+    });
   }
 
   @override
